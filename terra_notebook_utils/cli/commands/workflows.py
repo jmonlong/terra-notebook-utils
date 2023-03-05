f3c86f27c2bdd70f6b5f1db6689f0db5c837f404
Jean Monlong <jmonlong@ucsc.edu>
2023-03-04 16:39:56 -0800

    fix new tsv argument

--- terra_notebook_utils/cli/commands/workflows.py
+++ terra_notebook_utils/cli/commands/workflows.py
@@ -66,7 +66,7 @@ def get_workflow(args: argparse.Namespace):
 
 @workflow_cli.command("estimate-submission-cost", arguments={
     "--submission-id": dict(type=str, required=True),
-    "--tsv": dict(type=bool, default=False, required=False),
+    "--tsv": dict(type=bool, default=False, action='store_true', required=False, help='output in TSV format'),
     ** workspace_args
 })
 def estimate_submission_cost(args: argparse.Namespace):
